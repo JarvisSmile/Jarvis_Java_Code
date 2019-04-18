922adf641f96e3b14c6f400fc2b1b02431609a0b
Jarvis <Jarvis_Smile@outlook.com>
2019-04-18 22:53:21 +0800

    20190418更新

---
+++ 201903C/src/com/hugeyurt/homework/H04_BigIntegerApp.java
@@ -0,0 +1,11 @@
+package com.hugeyurt.homework;
+
+public class H04_BigIntegerApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		H04_BigIntegerClass B1=new H04_BigIntegerClass("111111111");
+		B1.add("222222");
+	}
+
+}
---
+++ 201903C/src/com/hugeyurt/homework/H04_BigIntegerClass.java
@@ -0,0 +1,21 @@
+package com.hugeyurt.homework;
+
+public class H04_BigIntegerClass 
+{
+	char []b;
+	public H04_BigIntegerClass(String s)
+	{
+		this.b=new char [s.length()];
+		this.b=s.toCharArray();
+	}
+	public char[] getb() {
+		return b;
+	}
+	public void setb(char[] b) {
+		this.b = b;
+	}
+	public void add(String s2)
+	{
+		
+	}
+}
